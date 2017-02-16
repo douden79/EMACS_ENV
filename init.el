2203ea9b7fe7accda84635289de13580956d9760
babel <kamase79@daum.net>
2017-02-16 18:42:45 +0900

    chnaged theme monokai.

--- init.el
+++ init.el
@@ -65,6 +65,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "adobe" :full normal :weight semi-bold :height 86 :width normal :full normal :full normal :full normal :full normal :full normal :full normal :full normal)))))
+ '(default ((t (:family "Source Code Pro" :foundry "adobe" :full normal :weight normal :height 80 :width normal :full normal :full normal :full normal :full normal :full normal :full normal :full normal :slant normal)))))
 (put 'downcase-region 'disabled nil)
 (put 'scroll-left 'disabled nil)
